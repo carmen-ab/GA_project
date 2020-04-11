c0a65dcf9e67941763c018fa8363076449623cbf
Carmen Abaurre <cabau@r206.uppmax.uu.se>
2020-04-11 18:49:56 +0200

    new canu attempt

--- code/01_batch_canu.sh
+++ code/01_batch_canu.sh
@@ -4,20 +4,19 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 18:00:00
-#SBATCH -J 05_assembly
+#SBATCH -J 07_assembly
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user carmen.abaor@gmail.com
 
 # Load modules
 
-module load-bioinfo-tools 
-module load canu\2.0
+module load-bioinfo-tools canu/2.0
 
 # Your commands
 
 canu \
--p canu -d ~/GA_project/analyses/01_assembly \
-genomeSize=2.5m \
--pacbio-raw ~/GA_project/data/DNA/DNA_raw_data/ERR2028*.fastq.gz \
+-p canu -d GA_project/analyses/01_assembly \
+genomeSize=2.6m \
+-pacbio-raw GA_project/data/DNA/DNA_raw_data/ERR2028*.fastq.gz \
 stopOnReadQuality=false \
 maxThreads=4
