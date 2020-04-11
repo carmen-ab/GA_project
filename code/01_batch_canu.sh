376263ba6301d6f8c310e57b1f4f5cf5695b41af
Carmen Abaurre <cabau@r206.uppmax.uu.se>
2020-04-11 18:26:12 +0200

    added batch script and data links

---
+++ code/01_batch_canu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 18:00:00
+#SBATCH -J 04_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user carmen.abaor@gmail.com
+
+# Load modules
+
+module load-bioinfo-tools canu\2.0
+
+# Your commands
+
+canu \
+-p canu -d ~/GA_project/analyses/01_assembly \
+genomeSize=2.5m \
+-pacbio-raw ~/GA_project/data/DNA/DNA_raw_data/ERR2028*.fastq.gz \
+stopOnReadQuality=false \
+corThreads=2
