0c4f53b7e0a0e47728c3e846fee98c7a88b59802
Carmen Abaurre <carmen.abaor@gmail.com>
2020-04-17 10:00:40 +0200

    batch job for prokka

---
+++ code/03_batch_prokka.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 1:00:00
+#SBATCH -J 01_annotation
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user carmen.abaor@gmail.com
+
+# Load modules
+
+module load bioinfo-tools prokka/
+
+# Your commands
+
+prokka \
+ --outdir analyses/03_annotation/01_prokka --force --prefix prokka_ \
+ --addgenes --genus Leptospirillum --species ferriphilum --strain DSM_14647 \
+ --kingdom Bacteria --gram neg --usegenus --proteins ??? --cpus ? --rfam \
+analyses/01_assembly/01_canu/canu.contigs.fasta
+
