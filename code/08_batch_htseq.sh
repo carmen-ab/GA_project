55ed413dede289541885a21351c10941d7aacb62
Carmen Abaurre <carmen.abaor@gmail.com>
2020-05-22 17:22:42 +0200

    added htseq batch job

--- code/08_batch_htseq.sh
+++ code/08_batch_htseq.sh
@@ -3,17 +3,17 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 16:00:00
-#SBATCH -J 01_counting
+#SBATCH -J 02_counting
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user carmen.abaor@gmail.com
 
 # Load modules
 
 module load bioinfo-tools
-module load  htseq
+module load  htseq/0.9.1
 
 # Your commands
 
 for i in {29..33};
-do htseq-counts -f bam -r pos -t CDS -i ID -s no "${i}.bam" annotation.gff > "${i}.txt";
+do htseq-count -f bam -r pos -t CDS -i ID -s no "${i}.bam" annotation.gff > "${i}.txt";
 done;
